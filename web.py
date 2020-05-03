f40460a5b0085d8d9c255a8ad1ff42dc1edbc597
deker104 <dshindov@protonmail.com>
2020-05-04 00:26:13 +0700

    add SQLALCHEMY_TRACK_MODIFICATIONS to suppress warning

--- web.py
+++ web.py
@@ -5,6 +5,7 @@ from config import DATABASE_URL
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
