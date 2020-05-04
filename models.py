01c8993986e9c88bfd3a70d7166945ef52fad4cb
deker104 <dshindov@protonmail.com>
2020-05-04 15:32:40 +0700

    add Procfile add SECRET_KEY add first model

---
+++ models.py
@@ -0,0 +1,7 @@
+from web import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
--- web.py
+++ web.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-from config import DATABASE_URL
+from config import DATABASE_URL, SECRET_KEY
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
