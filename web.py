24e973ab486d56105b5ec55f3d9a69f389acf149
Irbis888 <maxxka@outlook.com>
2020-05-05 11:37:51 +0700

    New pages

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 from config import DATABASE_URL, SECRET_KEY
@@ -11,5 +11,30 @@ db = SQLAlchemy(app)
 
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    template = {}
+    template["title"] = "Deliver"
+    template["pathcss"] = url_for('static', filename='css/style.css')
+    return render_template('index.html', **template)
+
+
+@app.route('/order')
+def order():
+    template = {}
+    template["title"] = "Заказ товаров"
+    template["pathcss"] = url_for('static', filename='css/ord.css')
+    return render_template('makeorder.html', **template)
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def reg():
+    template = {}
+    template["title"] = "Регистрация"
+    template["pathcss"] = url_for('static', filename='css/reg.css')
+    return render_template('reg.html', **template)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+
