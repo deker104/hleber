f26f3c3c6c9830d21046df50fb3d07408b495ea1
deker104 <dshindov@protonmail.com>
2020-05-05 23:08:36 +0700

    fix database, rework templates

--- models.py
+++ models.py
@@ -1,9 +1,7 @@
-# from web import db
-import sqlalchemy as db
-from db_session import SqlAlchemyBase
+from web import db
 
 
-class User(SqlAlchemyBase):
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String, nullable=False)
@@ -11,7 +9,7 @@ class User(SqlAlchemyBase):
     phone = db.Column(db.String, nullable=False)
 
 
-class Order(SqlAlchemyBase):
+class Order(db.Model):
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     order = db.Column(db.String, nullable=False)
--- web.py
+++ web.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-import db_session
-from models import *
 
 from config import DATABASE_URL, SECRET_KEY
 
@@ -9,49 +7,36 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-db_session.global_init("orders.sqlite")
-session = db_session.create_session()
-orders = session.query(Order).all()
-users = session.query(User).all()
-print(orders)
+db = SQLAlchemy(app)
 
+from models import *
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    template = {}
-    template["title"] = "Deliver"
-    template["pathcss"] = url_for('static', filename='css/style.css')
-    return render_template('index.html', **template)
+    return render_template('index.html')
 
 
 @app.route('/order')
 def order():
-    template = {}
-    template["title"] = "Заказ товаров"
-    template["pathcss"] = url_for('static', filename='css/ord.css')
-    return render_template('makeorder.html', **template)
+    return render_template('make_order.html')
 
 
 @app.route('/orders')
 def orders():
     template = {}
-    oi = ""
-    for i in orders:
-        oi += f"<p>{i.order}</p><p>{i.text}</p>"
-    template["ordinfo"] = oi
-
+    # oi = ""
+    # for i in orders:
+    #     oi += f"<p>{i.order}</p><p>{i.text}</p>"
+    # template["ordinfo"] = oi
+    # TODO
     return render_template('orders.html', **template)
 
 
 @app.route('/register', methods=['POST', 'GET'])
 def reg():
-    template = {}
-    template["title"] = "Регистрация"
-    template["pathcss"] = url_for('static', filename='css/reg.css')
-    return render_template('reg.html', **template)
+    return render_template('reg.html')
 
 
 if __name__ == '__main__':
