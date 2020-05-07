f98bd3e61d2195ad27cb5a03a55f5cf4ae5ca146
deker104 <dshindov@protonmail.com>
2020-05-07 11:48:02 +0700

    add dropdown menu for logged in users

--- migrate.py
+++ migrate.py
@@ -1,4 +1,13 @@
 from web import db
+from web import User
 
 db.drop_all()
 db.create_all()
+
+user = User(
+    id=463526827,
+    first_name='Даниил',
+    last_name='Шиндов'
+)
+db.session.add(user)
+db.session.commit()
--- web.py
+++ web.py
@@ -6,6 +6,7 @@ from flask import request
 from flask import session
 from flask import redirect
 from flask import url_for
+from flask import flash
 from flask_login import LoginManager
 from flask_login import login_required
 from flask_login import login_user
@@ -26,8 +27,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['USE_SESSION_FOR_NEXT'] = True
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'reg'
-login_manager.login_message = 'Вам необходимо войти для доступа к этой странице'
+login_manager.login_view = 'login'
+login_manager.login_message = 'Вам необходимо войти для доступа к этой странице.'
 
 from forms import MakeOrder
 from models import User
@@ -58,7 +59,8 @@ def make_order():
         )
         db.session.add(order)
         db.session.commit()
-        return request.form['address']
+        flash('Успешно добавлен новый заказ.')
+        return redirect(url_for('index'))
     form.phone.data = current_user.phone
     return render_template('make_order.html', form=form)
 
@@ -70,17 +72,17 @@ def orders():
     return render_template('orders.html', query=query)
 
 
-@app.route('/register')
-def reg():
-    return render_template('reg.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 @app.route('/vk_auth')
 def vk_auth():
-    h1 = request.args.get('hash')
+    hash_args = request.args.get('hash')
     user_id = request.args.get('uid')
-    h2 = md5(f'{VK_APP_ID}{user_id}{VK_SECRET_KEY}'.encode()).hexdigest()
-    if h1 == h2:
+    hash_computed = md5(f'{VK_APP_ID}{user_id}{VK_SECRET_KEY}'.encode()).hexdigest()
+    if hash_args == hash_computed:
         user = User.query.get(user_id)
         if user is None:
             user = User(
@@ -94,7 +96,26 @@ def vk_auth():
         next = session.get('next')
         if next is None or not is_safe_url(next):
             next = url_for('index')
+        flash('Вы вошли в свой аккаунт.')
         return redirect(next)
+
+
+@app.route('/test')
+def test():
+    user = User.query.get(463526827)
+    login_user(user)
+    flash('Вы вошли в свой аккаунт.')
+    return redirect(url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    next = request.referrer
+    if next is None or not is_safe_url(next):
+        next = url_for('index')
+    flash('Вы вышли из аккаунта.')
+    return redirect(next)
 
 
 if __name__ == '__main__':
