8f809a1840ce20616ad446a97efde6a88e15d3f7
deker104 <dshindov@protonmail.com>
2020-05-09 17:37:13 +0700

    fix migrate.py

--- migrate.py
+++ migrate.py
@@ -1,16 +1,20 @@
+from app import create_app
 from app import db
 from app.models import User
 
-user = User(
-    id=422289484,
-    first_name='Макс',
-    last_name='Зарянов'
-)
-db.session.add(user)
-user = User(
-    id=463526827,
-    first_name='Даниил',
-    last_name='Шиндов'
-)
-db.session.add(user)
-db.session.commit()
+app = create_app()
+
+with app.app_context():
+    user = User(
+        id=422289484,
+        first_name='Макс',
+        last_name='Зарянов'
+    )
+    db.session.add(user)
+    user = User(
+        id=463526827,
+        first_name='Даниил',
+        last_name='Шиндов'
+    )
+    db.session.add(user)
+    db.session.commit()
